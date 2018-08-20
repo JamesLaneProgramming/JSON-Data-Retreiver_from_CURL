1f3f6b61ffa73f76823731cd7f921ff4a8b9e3aa
jameslaneprogramming <jamesjrlane@gmail.com>
2018-08-20 13:38:30 +1000

    Fix disgusting JSON handling

--- Update_Emails.py
+++ Update_Emails.py
@@ -1,56 +1,55 @@
 #!usr/bin/python
 
+import os
 import sys
 import requests
 import json
 
+def get_bearer_token(_dir):
+    if os.path.exists(_dir):
+        with open(_dir, 'r') as token_file:
+            try:
+                print('Token file accessed and read')
+                file_content = token_file.read().splitlines()[0]
+            except IOError as e:
+                raise e
+                sys.exit()
+            except EOFError as e:
+                raise e
+                sys.exit()
+    return file_content
+
+def get_generated_emails(_dir):
+    if os.path.exists(_dir):
+        with open(_dir, 'r') as email_file:
+            try:
+                file_content = email_file.read().splitlines()[0]
+            except IOError as e:
+                raise e
+            except EOFError as e:
+                throw e
+    return file_content
+
 def main():
-    headers = {'Authorization' : 'Bearer ACTUAL_BEARER'}
+    bearer_token = get_bearer_token('./token')
+    headers = {'Authorization' : 'Bearer {0}'.format(bearer_token)}
     response = requests.get('https://coderacademy.instructure.com/api/v1/courses/92/users?per_page=100',
                 headers=headers)
 
-    #Convert response into string
-    responseText = response.text
-    #Remove JSON array symbols
-    responseText = responseText[1:]
-    responseText = responseText[:-2]
-    #Split JSON data for iteration purposes
-    responseText = responseText.split("},")
-    '''
-    Note: 
-    Splitting the string in this way will remove the REGEX expression from the
-    responseText and will need to be added manually later while iterating.
-    Removing the comma will also convert the response text into an array of
-    strings rather than an array of JSON data.
-    '''
-    currentUserID = ''
-    currentUserEmail = ''
-    for each_element in responseText:
-        #Append the JSON data so that we can parse it into a dictionary
-        each_element += "}"
-
+    json_data = json.loads(response.text)
+    for each_element in json_data:
         #Load the JSON data into a dictionary so that we can read key/value pairs
-        jsondata = json.loads(each_element)
-
-        #Iterate through the dictionary and find login_id.
-        
-        for key, value in jsondata.items():
+        for key, value in each_element:
             if(key == 'login_id'):
                 #print(value)
                 currentUserEmail = key
             if(key == 'id'):
                 currentUserID = key
-            
-            #Change newEmail to a curl request for google sheets.
-            newEmail = "james.lane@coderacademy.edu.au"
+            #Generate the new email address based on a lambda filter function
 
-            #Generate a the target url for the the put request using the userID
-            url = 'https://coderacademy.instructure.com/api/v1/users/{0}.json'.format(currentUserID)
-            #Create updated account details
-            parameters = {'user[email]':newEmail}
-            #Send request using the generated url and updated details
-            requests.put(url, headers=headers, data=parameters)
-
+def update_canvas_email(_student_ID, _email):
+    parameters = {'user[email]':_email}
+    url = 'https://coderacademy.instructure.com/api/v1/users/{0}.json'.format(student_ID)
+    requests.put(url, headers = headers, data = parameters)
 if __name__ == "__main__":
     main()
-
