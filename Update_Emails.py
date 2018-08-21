5e714f60dc894cbf91b4e4334497df31d73ba7cf
jameslaneprogramming <jamesjrlane@gmail.com>
2018-08-21 15:29:10 +1000

    Removed sensitive data into config variables

--- Update_Emails.py
+++ Update_Emails.py
@@ -43,6 +43,24 @@ def main():
     #Loads the config file
     config = get_config('./config.yaml')
 
+    #Canvas config variables
+    try:
+        request_parameters = config['canvas']['request_parameters']
+        course_ID = config['canvas']['course_id']
+        canvas_bearer_token = config['canvas']['bearer_token']
+    except KeyError as error:
+        print('Could not find config key specified')
+        raise error
+
+    #Google sheets config variables
+    try:
+        SPREADSHEET_ID = config['google_sheets']['spreadsheet_ID']
+        RANGE_NAME = config['google_sheets']['sheet_range']
+    except KeyError as error:
+        print('could not find config key specified')
+        raise error
+    
+    #Google credentials
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
@@ -50,34 +68,34 @@ def main():
         creds = tools.run_flow(flow, store)
     service = build('sheets', 'v4', http=creds.authorize(Http()))
 
-    # Call the Sheets API
-    SPREADSHEET_ID = '1FVjXzSXizzJNIFTuDRuuhVfNMq-qiKnbZq8aUuNUnaI'
-    RANGE_NAME = 'emails!A2:B82'
     result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
                                                 range=RANGE_NAME).execute()
-    values = result.get('values', [])
-
-    if not values:
+    sheet_data = result.get('values', [])
+    
+    #Check if the sheet_data is empty
+    if not sheet_data:
         print("No values found in spreadsheet, Exiting")
         sys.exit()
-    
-    course_ID = config['canvas']['course_id']
-    canvas_bearer_token = config['canvas']['bearer_token']
-    headers = {'Authorization' : 'Bearer{0}'.format(canvas_bearer_token)}
-    url = 'https://coderacademy.instructure.com/api/v1/courses/144/users?per_page=100'.format(course_ID)
-    response = requests.get(url, headers=headers)
 
-    json_data = json.loads(response.text)
+    #Canvas data request
+    headers = {'Authorization' : 'Bearer {0}'.format(canvas_bearer_token)}
+    url = 'https://coderacademy.instructure.com/api/v1/courses/{0}/users?{1}'.format(course_ID,
+                                                                              request_parameters)
+    response = requests.get(url, headers=headers)
+    #Load the request data into a JSON object
+    canvas_data = json.loads(response.text)
     
     #Create an array of dictionaries from google sheet values
     students = []
-    for each in values:
+
+    for each in sheet_data:
         students.append({"name": each[0], "email": each[1]})
     names = []
     for each in students:
         names.append(each['name'].lower())
-    print("{0} students extracted from spreadsheet".format(len(values)))
-    students_found = list(filter(lambda x: x['name'].lower() not in names, json_data))
+    print("{0} students extracted from spreadsheet".format(len(sheet_data)))
+    students_found = list(filter(lambda x: x['name'].lower() not in names,
+                                 canvas_data))
     #final = list(map(lambda x, y: update_canvas_email(x['id'], y['email'],
     #                                                  headers), students_found, students)
     print("{0} students after filter".format(len(students_found)))
@@ -87,7 +105,11 @@ def update_canvas_email(student_ID, email, _headers):
     parameters = {'user[email]':email}
     url = 'https://coderacademy.instructure.com/api/v1/users/{0}.json'.format(student_ID)
     update_request = requests.put(url, headers = _headers, data = parameters)
-    print(update_request)
+    if(update_request.status == 200):
+        print("Successfully updated canvas email")
+    else:
+        print("There was an error updating a canvas email", '\n')
+        print("Student with ID: {0} failed to update with error code: {1}".format(student_ID, update.request.status))
 
 if __name__ == "__main__":
     main()
