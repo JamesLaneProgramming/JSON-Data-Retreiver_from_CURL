9a81272283b6c2c080f7715abf5b9ddeddf06146
jameslaneprogramming <jamesjrlane@gmail.com>
2018-12-06 15:33:42 +1100

    Returned response text from student_search endpoint

--- application.py
+++ application.py
@@ -121,8 +121,9 @@ def student_search():
     if(request.method == 'POST'):
         try:
             search_term = request.args.get('search_term')
-            return canvas_API_request('https://coderacademy.instructure.com/api/v1/accounts/1/users', 
-                request_parameters={'search_term': search_term})
+            canvas_request = canvas_API_request('https://coderacademy.instructure.com/api/v1/accounts/1/users', 
+                    request_parameters={'search_term': search_term})
+            return canvas_request.text
         except Exception as error:
             raise error
     else:
