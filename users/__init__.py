9b04ff02cc0d7118a091991cabb19f3d91944e87
jameslaneprogramming <jamesjrlane@gmail.com>
2018-12-10 15:39:14 +1100

    Add __init__.py to users module for importing

---
+++ users/__init__.py
@@ -0,0 +1,2 @@
+#__init__.py is placed here to ensure that the this directory is appended to sys.path therefor allowing imports from other modules/packages.
+#https://docs.python.org/3/tutorial/modules.html
