54644ba2b03b50e8ec1731b8fe429c7624fc074a
jameslaneprogramming <jamesjrlane@gmail.com>
2018-12-12 14:27:22 +1100

    Fix missing comma's in MONGODB_SETTINGS object

--- application.py
+++ application.py
@@ -30,10 +30,10 @@ application = Flask(__name__, template_folder='templates')
 application.secret_key = 'super secret key'
 application.config['SESSION_TYPE'] = 'filesystem'
 application.config['MONGODB_SETTINGS'] = {
-    'db': 'canvas_integration'
-    'host': 'ds125684.mlab.com:25684'
-    'username': 'James'
-    'password': environ.get('mongoDB_Password')
+    'db': 'canvas_integration',
+    'host': 'ds125684.mlab.com:25684',
+    'username': 'James',
+    'password': environ.get('mongoDB_Password'),
     'authentication_source': 'canvas_integration'
         }
 db = MongoEngine(application)
