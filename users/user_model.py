9489f307e0c66c67162cf46d292209f72d91104f
jameslaneprogramming <jamesjrlane@gmail.com>
2018-12-13 09:26:42 +1100

    Fix issue where get_id would return incorrect id

--- users/user_model.py
+++ users/user_model.py
@@ -108,7 +108,7 @@ class User(Document):
             The ID associated with the User account.
         '''
         #TODO: Encode user id as unicode string
-        return str("ASDFASDF")
+        return str(self._id)
 
     def get(_id):
         '''
