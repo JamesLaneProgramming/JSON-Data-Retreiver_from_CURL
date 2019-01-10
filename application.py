91596c6a8feb7946d0e48fcd4392c1cfed3f6ae6
jameslaneprogramming <jamesjrlane@gmail.com>
2019-01-10 12:16:24 +1100

    Add validation to learning_outcome creation

--- application.py
+++ application.py
@@ -155,9 +155,9 @@ def map_rubric_data(submission_data):
                                           submission_rubric_assessment)
             submission_grades = []
             for each_criteria in submission.criteria:
-                learning_outcome = Learning_Outcome(int(each_criteria.id),
-                                                           float(each_criteria.points))
-                learning_outcome.save()
+                if(each_criteria != None and each_criteria.points != None):
+                    learning_outcome = Learning_Outcome(int(each_criteria.id),
+                                                               float(each_criteria.points)).save()
                 submission_grades.append(learning_outcome)
             Assessment.create(each_submission_item['user_id'], submission_grades)
 
