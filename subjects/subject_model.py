242c0b086fdddf4377921b4de785bf06acb3f872
jameslaneprogramming <jamesjrlane@gmail.com>
2019-01-21 10:45:03 +1100

    Fix: rename learning_outcome document reference to learning_outcomes

--- subjects/subject_model.py
+++ subjects/subject_model.py
@@ -3,7 +3,7 @@ from mongoengine import Document, StringField, ListField, ReferenceField
 class Subject(Document):
     subject_name = StringField(required=True)
     subject_description = StringField()
-    learning_outcomes = ListField(ReferenceField('learning_outcome'))
+    learning_outcomes = ListField(ReferenceField('learning_outcomes'))
     
     def read():
         return Subject.objects()
