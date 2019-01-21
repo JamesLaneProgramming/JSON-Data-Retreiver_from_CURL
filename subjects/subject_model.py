8715e793064268bfd4d5b735126e1141a5b82b65
jameslaneprogramming <jamesjrlane@gmail.com>
2019-01-21 11:16:40 +1100

    Pass in learning outcome document class to subject reference field constructor

--- subjects/subject_model.py
+++ subjects/subject_model.py
@@ -4,7 +4,7 @@ from learning_outcomes.learning_outcome_model import Learning_Outcome
 class Subject(Document):
     subject_name = StringField(required=True)
     subject_description = StringField()
-    learning_outcomes = ListField(ReferenceField('learning_outcomes'))
+    learning_outcomes = ListField(ReferenceField(Learning_Outcome))
     
     def read():
         return Subject.objects()
