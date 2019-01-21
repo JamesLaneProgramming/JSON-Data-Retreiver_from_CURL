98df1b5f8f2b6bd2af046eb81a433a75e590d1d1
jameslaneprogramming <jamesjrlane@gmail.com>
2019-01-21 17:43:08 +1100

    Add logging to subject index method

--- subjects/subject_model.py
+++ subjects/subject_model.py
@@ -13,6 +13,7 @@ class Subject(Document):
     
     def index(id):
         o_id = mongo_methods.generate_objectid_from_string(id)
+        print(o_id)
         return Subject.objects(pk=o_id)
 
     def delete(id):
