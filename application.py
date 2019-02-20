fabf3d8d9a813118ddaa69b09dfcee2359e4fe44
jameslaneprogramming <jamesjrlane@gmail.com>
2019-02-20 11:29:10 +1100

    Update print syntax

--- application.py
+++ application.py
@@ -504,7 +504,7 @@ def create_canvas_account():
     if(creation_response.status_code == 400):
         print("The user already exists")
         existing_user_details = search_students(student_email)
-        print("User details: " type(existing_user_details))
+        print("User details: ", type(existing_user_details))
         existing_user_id = existing_user_details['id']
         enroll_canvas_student(existing_user_id, course_ID)
         return "User already exists, user not enrolled"
