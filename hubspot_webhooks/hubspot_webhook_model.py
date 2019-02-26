0c0ffbefe7b24476e5ad4b1db42561040f83d8f7
jameslaneprogramming <jamesjrlane@gmail.com>
2019-02-26 16:50:18 +1100

    Try this

--- hubspot_webhooks/hubspot_webhook_model.py
+++ hubspot_webhooks/hubspot_webhook_model.py
@@ -7,7 +7,5 @@ class Hubspot_Webhook(DynamicDocument):
     meta = {'collection': 'hubspot_webhooks'}
 
     def create(json_data):
-        for each in json_data:
-            json_element = json.loads(each)
-            hubspot_webhook = Hubspot_Webhook(**json_element).save()
-            return hubspot_webhook
+        hubspot_webhook = Hubspot_Webhook(**json_data).save()
+        return hubspot_webhook
