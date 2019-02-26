248b460d4361571044ca35850b9948a9362b5db4
jameslaneprogramming <jamesjrlane@gmail.com>
2019-02-26 17:00:18 +1100

    Loop json data and save to db

--- hubspot_webhooks/hubspot_webhook_model.py
+++ hubspot_webhooks/hubspot_webhook_model.py
@@ -7,5 +7,6 @@ class Hubspot_Webhook(DynamicDocument):
     meta = {'collection': 'hubspot_webhooks'}
 
     def create(json_data):
-        hubspot_webhook = Hubspot_Webhook(**json_data)
-        return hubspot_webhook
+        for each in json_data:
+            hubspot_webhook = Hubspot_Webhook(**each)
+            return hubspot_webhook
