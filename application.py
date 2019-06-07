b18ada98d6a5419a3fefabf693af80be673a360a
jameslaneprogramming <jamesjrlane@protonmail.com>
2019-06-07 14:40:28 +1000

    Print cookies for testing

--- application.py
+++ application.py
@@ -213,6 +213,7 @@ def require_hubspot_signature_validation(func):
 def require_hubspot_access_token(func):
     @wraps(func)
     def update_hubspot_access_token(*args, **kwargs):
+        print(request.cookies)
         if request.cookies.get('hubspot_access_token') is not None:
             '''
             https://tools.ietf.org/html/rfc6749#section-1.5
