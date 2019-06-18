10d794d2eafce0cd2ce78e36c5f199c8e4e34203
jameslaneprogramming <jamesjrlane@gmail.com>
2019-06-18 19:13:26 +1000

    Test

--- application.py
+++ application.py
@@ -754,7 +754,7 @@ def retreive_rubric_assessment():
 def student_subject_grades():
     if(request.method == 'GET'):
         try:
-            distinct_graded_users = Grade.objects().distinct('canvas_user_id')
+            distinct_graded_users = Grade.objects().select_related()            
             for user in distinct_graded_users:
                 print(user.canvas_user_id)
                 grades = Grade.objects(canvas_user_id=user.canvas_user_id)
